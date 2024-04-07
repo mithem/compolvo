fe6be12b60df2ed20c3321e889de8706238d45d5
mithem <mithem@github.com>
2024-04-07 19:37:40 +0200

    WIP: API security (RBAC), dockerization, more API commands (for users (previously customers))

--- src/server/compolvo/models.py
+++ src/server/compolvo/models.py
@@ -1,16 +1,80 @@
+import asyncio
 from enum import IntEnum
+from typing import List, Type
 
+from sanic import json
 from tortoise.models import Model
 from tortoise.fields import UUIDField, TextField, CharField, IntEnumField, FloatField, IntField, DatetimeField, BooleanField, ForeignKeyField
+from tortoise import run_async
+
+class Serializable:
+    fields: List[str]
+
+    def to_dict(self):
+        data = {}
+        for field in self.fields:
+            value = getattr(self, field)
+            data[field] = value
+        return data
+
+    def json(self):
+        return json(self.to_dict())
+
+    @staticmethod
+    def list_json(objects: List["Serializable"]):
+        return json(list(map(lambda obj: obj.to_dict(), objects)))
+
+    @staticmethod
+    async def all_json(cls: Type[Model]):
+        assert Serializable in cls.__mro__, "`cls` must be a subclass of `Serializable`."
+        assert Model in cls.__mro__, "`cls` must be a subclass of `Model`."
+        return cls.list_json(await cls.all()) # typing: ignore
 
 
-class Customer(Model):
+class User(Model, Serializable):
     id = UUIDField(pk=True)
     name = TextField(null=True)
     email = CharField(255, unique=True)
-    password_hash = TextField(null=True)
+    password = TextField(null=True)
 
-class Service(Model):
+    fields = ["name", "email", "password"]
+
+    @property
+    def rolenames(self):
+        return ["user"] # TODO: Find a way to actually get user's roles
+        return list(map(lambda role: role.role, run_async(UserRole.filter(user=self.id).all())))
+
+    @classmethod
+    async def lookup(cls, username=None, email=None):
+        if username is not None:
+            return await cls.filter(name=username).get_or_none()
+        elif email is not None:
+            return await cls.filter(email=email).get_or_none()
+        else:
+            return None
+
+    @classmethod
+    async def identify(cls, id):
+        return await cls.filter(id=id).get_or_none()
+
+    @property
+    def identity(self):
+        return self.id
+
+
+
+class UserRole(Model, Serializable):
+    class Role(IntEnum):
+        USER = 1
+        ADMIN = 2
+
+    id = UUIDField(pk=True)
+    user = ForeignKeyField("models.User", "roles")
+    role = IntEnumField(Role)
+
+    fields = ["user", "role"]
+
+class Service(Model, Serializable):
     class RetrievalMethod(IntEnum):
         COMMAND = 1
         APT = 2
@@ -21,7 +85,9 @@ class Service(Model):
     retrieval_data = TextField()
     latest_version = TextField(null=True)
 
-class ServiceOffering(Model):
+    fields = ["retrieval_method", "retrieval_data", "latest_version"]
+
+class ServiceOffering(Model, Serializable):
     id = UUIDField(pk=True)
     name = TextField()
     service = ForeignKeyField("models.Service", "service_offerings")
@@ -29,24 +95,32 @@ class ServiceOffering(Model):
     price = FloatField()
     duration_days = IntField()
 
-class ServicePlan(Model):
+    fields = ["name", "service", "description", "price", "duration_days"]
+
+class ServicePlan(Model, Serializable):
     id = UUIDField(pk=True)
     service_offering = ForeignKeyField("models.ServiceOffering", "service_plans")
-    customer = ForeignKeyField("models.Customer")
+    user = ForeignKeyField("models.User")
     start_date = DatetimeField()
     end_date = DatetimeField(null=True)
-    canceledByUser = BooleanField(default=False)
+    canceled_by_user = BooleanField(default=False)
 
-class Payment(Model):
+    fields = ["service_offering", "user", "start_date", "end_date", "canceled_by_user"]
+
+class Payment(Model, Serializable):
     id = UUIDField(pk=True)
     service_plan = ForeignKeyField("models.ServicePlan", "payments")
     amount = FloatField()
     date = DatetimeField()
 
-class Agent(Model):
+    fields = ["service_plan", "amount", "date"]
+
+class Agent(Model, Serializable):
     id = UUIDField(pk=True)
-    customer = ForeignKeyField("models.Customer", "agents")
+    user = ForeignKeyField("models.User", "agents")
     last_connection_start = DatetimeField(null=True)
+
+    fields = ["user", "last_connection_start"]
 
 class AgentSoftware(Model):
     id = UUIDField(pk=True)
@@ -54,3 +128,5 @@ class AgentSoftware(Model):
     service_plan = ForeignKeyField("models.ServicePlan", "agent_softwares")
     installed_version = TextField(null=True)
     corrupt = BooleanField(default=False)
+
+    fields = ["agent", "service_plan", "installed_version", "corrupt"]
--- src/server/server.py
+++ src/server/server.py
@@ -1,30 +1,124 @@
-from sanic import Sanic, redirect, json
+import secrets
+import string
+
+import sanic_beskar
+from sanic import Sanic, redirect, HTTPResponse, Request, text, json
+from sanic.errorpages import BadRequest
+from sanic_beskar import Beskar
+from sanic_beskar.exceptions import AuthenticationError, TOTPRequired
 from tortoise.contrib.sanic import register_tortoise
 from tortoise.exceptions import IntegrityError
 
-from compolvo.models import Customer
+from compolvo.models import User, Service, Serializable
 
 app = Sanic("compolvo")
+beskar = Beskar()
 
-register_tortoise(app, db_url='mysql://root:@192.168.1.101:3306/compolvo', modules={'models': ['compolvo.models']}, generate_schemas=True)
+#app.config.SECRET_KEY = "".join(secrets.choice(string.ascii_letters) for i in range(32))
+app.config.SECRET_KEY = "helloworld"
+
+beskar.init_app(app, User)
+register_tortoise(app, db_url='mysql://root:@localhost:3306/compolvo',
+                  modules={'models': ['compolvo.models']}, generate_schemas=True)
+
 
 @app.get("/")
 async def index(request):
-    return redirect("/api/customer")
-
-@app.get("/api/customer")
-async def get_customers(request):
-    return json(list(map(lambda c: dict(c), await Customer.all())))
+    return redirect("/docs")
 
 
-@app.post("/api/customer")
-async def create_customer(request):
+@app.listener("before_server_start")
+async def test_user(request):
+    email = "test@example.com"
+    user = await User.get_or_none(email=email)
+    if user:
+        return text("Already exists.")
+    else:
+        await User(
+            email=email,
+            name="Test user",
+            password=beskar.hash_password("test")
+        ).save()
+        return text("Created", status=201)
+
+
+@app.get("/login")
+async def login(request: Request):
     try:
-        customer = Customer(name=request.json["name"], email=request.json["email"])
-        await customer.save()
-        return json(dict(customer))
-    except IntegrityError as e:
-        return json(dict(error=str(e)), status=500)
+        user = await beskar.authenticate(request.args["email"][0], request.args["password"][0],
+                                         lookup="email")
+        return redirect(
+            request.url_for("index"),
+            headers={"Authorization": "Bearer " + await beskar.encode_jwt_token(user)},
+        )
+    except (KeyError, IndexError):
+        raise BadRequest("Expected email as well as password.")
+    except (AuthenticationError, TOTPRequired) as e:
+        raise BadRequest("Invalid email or password.") from e
+
+@app.get("/protected")
+@sanic_beskar.auth_required
+async def protected(request):
+    return text("You're accessing a protected page!")
+
+
+@app.get("/api/user")
+@sanic_beskar.roles_required(["admin"])
+async def get_users(request):
+    return await Serializable.all_json(User)
+
+
+@app.post("/api/user")
+async def create_user(request):
+    try:
+        user = User(
+            name=request.json["name"],
+            email=request.json["email"],
+            password=beskar.hash_password(request.json["password"])
+        )
+        await user.save()
+        return user.json()
+    except KeyError:
+        raise BadRequest("Missing name, email, or password.")
+    except IntegrityError:
+        raise BadRequest("User with specified email already exists.")
+
+
+@app.patch("/api/user")
+async def update_user(request):
+    # TODO: Authentication: require that user changes itself or has admin/user role
+    try:
+        old_email = request.json["email"]
+        new_name = request.json.get("new_name", None)
+        new_email = request.json.get("new_email", None)
+        new_password = request.json.get("new_password", None)
+        user = await User.get(email=old_email)
+        if new_name is not None:
+            user.name = new_name
+        if new_email is not None:
+            user.email = new_email
+        if new_password is not None:
+            user.password = beskar.hash_password(new_password)
+        await user.save()
+        return user.json()
+    except KeyError:
+        raise BadRequest("Please specify the old email of the user to change its attributes of.")
+
+
+@app.delete("/api/user")
+async def delete_user(request):
+    try:
+        email = request.json["email"]
+        user = await User.get(email=email)
+        await user.delete()
+        return HTTPResponse(status=204)
+    except KeyError:
+        raise BadRequest("Please specify the new email of the user to delete.")
+
+
+@app.get("/api/service")
+async def get_services(request):
+    return await Serializable.all_json(Service)
 
 
 if __name__ == "__main__":
