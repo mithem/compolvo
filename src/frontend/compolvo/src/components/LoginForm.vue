bb1fb95d37916417dba0933c05af4c01932e3b67
mithem <mithem@github.com>
2024-04-14 08:55:22 +0200

    Fully featured login/logout flow
    - use production build in frontend image
    - delete old docker-composes for frontend and server

--- src/frontend/compolvo/src/components/LoginForm.vue
+++ src/frontend/compolvo/src/components/LoginForm.vue
@@ -1,37 +1,79 @@
 <template>
-  <v-col>
-    <v-text-field
-      v-model="email"
-      :counter="50"
-      label="E-Mail"
-      hide-details
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :counter="50"
-      label="Password"
-      hide-details
-      required
-    >
-    </v-text-field>
-    <v-btn @click="login">Submit</v-btn>
-  </v-col>
+  <div class="card-container">
+    <v-card class="form-card">
+      <v-form @submit.prevent="login">
+        <v-col>
+          <v-text-field
+            class="vert-input-field"
+            v-model="email"
+            :counter="50"
+            label="E-Mail"
+            hide-details
+            required
+          ></v-text-field>
+          <v-text-field
+            class="vert-input-field"
+            v-model="password"
+            :counter="50"
+            label="Password"
+            hide-details
+            required
+          >
+          </v-text-field>
+          <v-btn
+            type="submit"
+            class="vert-input-field"
+          >Submit
+          </v-btn>
+        </v-col>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 
 <style scoped>
+.form-card {
+  background-color: #f7f7f7;
+  border: none;
+  border-radius: 10px;
+  padding: 10px;
+  max-width: 400px;
+  width: 40%;
+  min-width: 200px;
+  margin: 50px auto;
+}
+
+.vert-input-field {
+  margin-bottom: 12px;
+}
+
+.card-container {
+  width: 100%;
+  height: 100%;
+}
 </style>
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 import Constants from "./Constants";
+
+const email = ref("")
+const password = ref("")
 
 defineComponent({
   name: 'LoginForm',
   setup() {
-  }
+    return {email, password}
+  },
 });
 
-function login() {
-  window.location.href = Constants.HOST_URL + "/api/login?redirect_url=" + Constants.HOST_URL + "&email=" + encodeURIComponent("test@example.com") + "&password=" + encodeURIComponent("test");
+async function login() {
+  const params = new URLSearchParams(window.location.search)
+  const redirectUrl = params.get("redirect_url") || Constants.HOST_URL
+  const res = await fetch(Constants.HOST_URL + "/api/login?redirect_url=" + redirectUrl + "&email=" + encodeURIComponent(email.value) + "&password=" + encodeURIComponent(password.value));
+  if (res.ok) {
+    document.location.href = redirectUrl
+  } else {
+    alert(await res.text())
+  }
 }
 </script>
--- src/frontend/compolvo/src/typed-router.d.ts
+++ src/frontend/compolvo/src/typed-router.d.ts
@@ -17,6 +17,7 @@ declare module 'vue-router/auto-routes' {
     '/detail': RouteRecordInfo<'/detail', '/detail', Record<never, never>, Record<never, never>>,
     '/home': RouteRecordInfo<'/home', '/home', Record<never, never>, Record<never, never>>,
     '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
+    '/logout': RouteRecordInfo<'/logout', '/logout', Record<never, never>, Record<never, never>>,
     '/profile': RouteRecordInfo<'/profile', '/profile', Record<never, never>, Record<never, never>>,
   }
 }
